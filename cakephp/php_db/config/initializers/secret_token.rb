a76f7abe56bdf66d34c41fe4c136e796d377677a
mouse <linhongshimouse@gmail.com>
2011-04-22 17:06:33 +0800

    添加rails 的migration 脚本，用于操作数据库

---
+++ cakephp/php_db/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhpDb::Application.config.secret_token = 'bfe20c26924353d7b71d31aa14c56cb21b3007fee50d390d2a1ec0c1bda4b3f7ccb6c1136f67bc5eb7cd445302067b70d29410eb82f02528a0704ab32f42e791'
